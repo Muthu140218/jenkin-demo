4d9ec2750eff197e75a729591f52c93dc09ef5b9
muthu <muthukumar.mani86@gmail.com>
2020-05-07 13:31:39 +0530

    updated jar name and new controller

---
+++ src/main/java/com/example/jenkin/JenkinDemoDockerController.java
@@ -0,0 +1,16 @@
+package com.example.jenkin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class JenkinDemoDockerController {
+
+	@GetMapping
+	public String index()
+	{
+		return "My Docker example";
+	}
+}
