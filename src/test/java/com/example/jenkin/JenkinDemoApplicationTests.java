860a937fab59391de23318ccb830536bec3edc05
muthu <muthukumar.mani86@gmail.com>
2020-05-07 09:54:39 +0530

    First Commit

---
+++ src/test/java/com/example/jenkin/JenkinDemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example.jenkin;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDemoApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinDemoApplicationTests.class);
+
+	
+	@Test
+	void contextLoads() {
+		logger.info("Test Cases executing....");
+		assertEquals(true, true);2
+	}
+
+}
